3d97244224e0583a2f305f69e1b6e97c7ad80e4d
Cosmitar <mhherrera31@gmail.com>
2025-02-03 07:33:32 +0100

    dev: add class for supporting change listeners in Blackboard

---
+++ src/enhanced/BlackboardWithListeners.ts
@@ -0,0 +1,32 @@
+import type Event from '../utils/Event';
+
+export default class BlackboardWithListeners<T extends Record<string, unknown>> {
+  private target: T;
+
+  constructor(initialObject: T, cb?: (key: keyof T, bb: T) => void) {
+    this.target = initialObject;
+
+    return new Proxy(this.target, {
+      get: (target, property, receiver) => {
+        if (Object.getOwnPropertyNames(this).includes(String(property))) {
+          // If the property exists in the class, access it
+          return Reflect.get(this, property, receiver);
+        }
+
+        return Reflect.get(target, property, receiver); // Access the proxied object
+      },
+
+      set: (target, property, value) => {
+        if (value !== target[property as keyof typeof target]) {
+          Reflect.set(target, property, value);
+
+          cb?.(property as keyof T, target);
+        }
+
+        return true;
+      },
+
+      has: (target, property) => Reflect.has(target, property)
+    }) as any as BlackboardWithListeners<T> & { ON_CHANGE: Event<[key: keyof T, bb: T]> };
+  }
+}
