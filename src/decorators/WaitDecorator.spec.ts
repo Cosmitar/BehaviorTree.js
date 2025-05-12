d7c5cc10676337de24e8f3c689c93cfd90eaae2d
Cosmitar <mhherrera31@gmail.com>
2025-05-12 21:54:08 +0200

    add tests for WaitDecorator

--- src/decorators/WaitDecorator.spec.ts
+++ src/decorators/WaitDecorator.spec.ts
@@ -1,85 +1,91 @@
-import WaitDecorator from './WaitDecorator';
-import { RUNNING, SUCCESS, FAILURE } from '../constants';
 import sinon from 'sinon';
+import { RUNNING, SUCCESS } from '../constants';
 import Task from '../Task';
 import type { Blackboard } from '../types';
+import WaitDecorator, { ERROR_NOOP_RUN, ERROR_SETTING_ON_WAITING } from './WaitDecorator';
 
 describe('WaitDecorator', () => {
   let clock: sinon.SinonFakeTimers;
   let decoratedTask: WaitDecorator;
-  let decoratedTask0s: WaitDecorator;
-  let decoratedSwitchTask: WaitDecorator;
   let blackboard: Blackboard;
-  // let runStub: sinon.SinonStub;
+
   const task = new Task({
-    run(blackboard) {
-      ++blackboard.count;
+    start(blackboard) {
+      ++blackboard.starts;
       return SUCCESS;
-    }
-  });
-  const switchTask = new Task({
-    run(blackboard) {
-      ++blackboard.count;
-      return blackboard.switchResult;
+    },
+    end(blackboard) {
+      ++blackboard.ends;
+      return SUCCESS;
     }
   });
 
   beforeEach(() => {
     clock = sinon.useFakeTimers();
     blackboard = {
-      count: 0
+      starts: 0,
+      ends: 0
     };
-    decoratedTask = new WaitDecorator({ config: { awaitFor: 5 }, node: task });
-    decoratedTask0s = new WaitDecorator({ config: { awaitFor: 0 }, node: task });
-    decoratedSwitchTask = new WaitDecorator({ config: { awaitFor: 5 }, node: switchTask });
+
+    decoratedTask = new WaitDecorator({ config: { awaitFor: 0 }, node: task });
   });
 
   afterEach(() => {
     clock.restore();
   });
 
-  it('should return result on first call when awaitFor is 0', () => {
-    const result = decoratedTask0s.run(blackboard);
+  it('should run end on first call when awaitFor is 0', () => {
+    const result = decoratedTask.run(blackboard);
     expect(result).toBe(SUCCESS);
-    expect(blackboard.count).toEqual(1);
+    expect(blackboard.starts).toEqual(1);
+    expect(blackboard.ends).toEqual(1);
   });
 
-  it('should return RUNNING when first called', () => {
-    const result = decoratedTask.run(blackboard);
+  it('should return RUNNING after first called', () => {
+    const result = decoratedTask.setWaitFor(5).run(blackboard);
     expect(result).toBe(RUNNING);
-    expect(blackboard.count).toEqual(0);
+    expect(blackboard.starts).toEqual(1);
+    expect(blackboard.ends).toEqual(0);
   });
 
   it('should return RUNNING if called before await time is up', () => {
-    decoratedTask.run(blackboard);
+    decoratedTask.setWaitFor(5).run(blackboard);
     clock.tick(4000); // 4 seconds
     const result = decoratedTask.run(blackboard);
     expect(result).toBe(RUNNING);
-    expect(blackboard.count).toEqual(0);
+    expect(blackboard.starts).toEqual(1);
+    expect(blackboard.ends).toEqual(0);
   });
 
   it('should call the run callback after await time is up', () => {
-    decoratedTask.run(blackboard);
+    decoratedTask.setWaitFor(5).run(blackboard);
     clock.tick(6000); // 6 seconds
     const result = decoratedTask.run(blackboard);
     expect(result).toBe(SUCCESS);
-    expect(blackboard.count).toEqual(1);
+    expect(blackboard.starts).toEqual(1);
+    expect(blackboard.ends).toEqual(1);
   });
 
   it('should reset awaiting state after running the node', () => {
-    decoratedTask.run(blackboard);
+    decoratedTask.setWaitFor(5).run(blackboard);
+    expect(decoratedTask.isWaiting).toBe(true);
     clock.tick(6000); // 6 seconds
     decoratedTask.run(blackboard);
     expect(decoratedTask.isWaiting).toBe(false);
   });
 
-  it('should return the result of the run callback after await time is up', () => {
-    decoratedSwitchTask.run(blackboard);
-    clock.tick(6000); // 6 seconds
+  it('should throw an error if the node has a run implementation', () => {
+    const taskWithRun = new Task({
+      run: () => {
+        return false;
+      }
+    });
+    expect(() => new WaitDecorator({ node: taskWithRun })).toThrow(ERROR_NOOP_RUN);
+  });
 
-    blackboard.switchResult = FAILURE;
-
-    const result = decoratedSwitchTask.run(blackboard);
-    expect(result).toBe(FAILURE);
+  it('should throw an error if setting waitFor while waiting', () => {
+    decoratedTask.setWaitFor(5).run(blackboard);
+    decoratedTask.run(blackboard);
+    expect(() => decoratedTask.setWaitFor(5)).toThrow(ERROR_SETTING_ON_WAITING);
   });
 });
