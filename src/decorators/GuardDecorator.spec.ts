8cbf0860b757bfe1417557f63c650de0fe2019aa
Cosmitar <mhherrera31@gmail.com>
2025-05-12 21:54:27 +0200

    add Tests for guard with GuardDecorator

---
+++ src/decorators/GuardDecorator.spec.ts
@@ -0,0 +1,80 @@
+import { expect } from '@jest/globals';
+import { FAILURE, RUNNING, SUCCESS } from '../constants';
+import Task from '../Task';
+import type { Blackboard, RunResult } from '../types';
+import GuardDecorator from './GuardDecorator';
+
+describe('GuardDecorator', () => {
+  let guardedTask: GuardDecorator<Blackboard>;
+  let guardedDefaultTask: GuardDecorator<Blackboard>;
+  let blackboard: Blackboard;
+
+  const taskNextResult = new Task({
+    run(blackboard) {
+      ++blackboard.count;
+      return blackboard.nodeNextResult;
+    }
+  });
+
+  beforeEach(() => {
+    blackboard = {
+      shouldPass: true, // guard condition
+      nodeNextResult: SUCCESS, // task result
+      count: 0
+    };
+
+    guardedTask = new GuardDecorator({ config: { condition: (bb) => bb.shouldPass }, node: taskNextResult });
+    guardedDefaultTask = new GuardDecorator({ config: {}, node: taskNextResult });
+  });
+
+  it('should return node result when condition is not provided', () => {
+    blackboard.nodeNextResult = SUCCESS;
+    const result = guardedDefaultTask.run(blackboard);
+
+    expect(result).toBe(SUCCESS);
+  });
+
+  it('should return node result when condition matches', () => {
+    blackboard.nodeNextResult = RUNNING;
+    blackboard.shouldPass = true;
+    const result = guardedTask.run(blackboard);
+
+    expect(result).toBe(RUNNING);
+  });
+
+  it('should return FAILURE when condition does not match', () => {
+    blackboard.nodeNextResult = SUCCESS; // guard will fail and this will be ignored
+    blackboard.shouldPass = false;
+    const result = guardedTask.run(blackboard);
+
+    expect(result).toBe(FAILURE);
+  });
+
+  it('should emulate moveTo node', () => {
+    const bb: Blackboard<{
+      targetToMove?: { x: number; y: number };
+      nodeNextResult: RunResult;
+      count: number;
+    }> = {
+      targetToMove: { x: 0, y: 0 }, // has target to move
+      nodeNextResult: RUNNING, // task will call to move and return this
+      count: 0
+    };
+
+    guardedTask = new GuardDecorator({ config: { condition: (bb) => bb.targetToMove !== undefined }, node: taskNextResult });
+    const result = guardedTask.run(bb);
+
+    expect(result).toBe(RUNNING);
+    expect(bb.count).toBe(1);
+
+    // at some point the target is reached and removed from the blackboard
+    bb.targetToMove = undefined;
+    bb.nodeNextResult = SUCCESS; // guard will fail and this will be ignored
+
+    // the tree run is called arbitrarily
+    const result2 = guardedTask.run(bb);
+
+    expect(result2).toBe(FAILURE);
+    expect(bb.count).toBe(1);
+  });
+});
